6e709e5fc459ad943f6e117b3455c20f589b0d07
amansingh-21 <amanofficial741@gmail.com>
2026-05-24 10:44:49 +0530

    feat: add header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here</h2>
+            <p>Enjoy a rich selection of expertly crafted dishes made with fresh ingredients and authentic flavors. Our mission is to bring joy to your table with every bite you take.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
