458275a7979cb48025ca2312610ef18dcd391f69
amansingh-21 <amanofficial741@gmail.com>
2026-06-09 21:04:49 +0530

    feat: add fooddisplay and fooditem component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { StoreContext } from './context/StoreContext.jsx'
+import { food_list } from './assets/assets.js'
 
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
+   <StoreContext.Provider value={{food_list}}>
       <App />
+   </StoreContext.Provider>
    </BrowserRouter>
 )
